338bd8cdd57b7157fd25529b546c8599a5b0dcec
Gabriel Cordeiro Barroso Teles <gabrielpikachuoficial@gmail.com>
2024-11-29 20:46:04 -0300

    Hello World!

---
+++ HelloWorld!/primeiro.go
@@ -0,0 +1,15 @@
+// Está falando pro Go que esse arquivo esta dentro do pacote main
+package main
+
+// Lista de imports (referenciar os pacotes que voce vai usar nesse arquivo)
+import (
+	"fmt"
+)
+	
+func main() {
+	fmt.Println("Olá mundo!")
+}
+// Como execultar?
+// Digita no terminal...
+// go run primeiro.go
+
