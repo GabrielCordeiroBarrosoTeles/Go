fd93f3b5ba416077a080597a17b9ef80bbd8bb14
Gabriel Cordeiro Barroso Teles <gabrielpikachuoficial@gmail.com>
2024-11-29 20:01:40 -0300

    Olá mundo

---
+++ Aula 1/primeiro.go
@@ -0,0 +1,13 @@
+// Está falando pro Go que esse arquivo esta dentro do pacote main
+package main
+
+// Lista de imports (referenciar os pacotes que voce vai usar nesse arquivo)
+import (
+	"fmt"
+)
+	
+func main() {
+	fmt.Println("Olá mundo!")
+}
+// go run primeiro.go
+
